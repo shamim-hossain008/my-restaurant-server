0385a4fc7ae635101bd35837195deaeeb4a8c708
shamim-hossain008 <shamim.hossain3573@gmail.com>
2024-06-29 19:58:21 +0800

    get data from server by user email

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Pass}@cluster0.7nyeh0h.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7nyeh0h.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +23,58 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+
+    const menuCollection = client.db("restaurantDB").collection("menu");
+    const reviewCollection = client.db("restaurantDB").collection("reviews");
+    const cartCollection = client.db("restaurantDB").collection("carts");
+
+    // get all menu data from server
+    app.get("/menu", async (req, res) => {
+      try {
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error.massage);
+      }
+    });
+
+    // get all reviews data
+    app.get("/reviews", async (req, res) => {
+      try {
+        const result = await reviewCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error.massage);
+      }
+    });
+
+    //  all carts collection
+
+    // get data from server
+    app.get("/carts", async (req, res) => {
+      try {
+        const email = req.query.email;
+        const query = { email: email };
+        const result = await cartCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error.message);
+      }
+    });
+
+    // save to server
+    app.post("/carts", async (req, res) => {
+      try {
+        const cartItem = req.body;
+        const result = await cartCollection.insertOne(cartItem);
+        res.send(result);
+        console.log(result, "cart Item");
+      } catch (error) {
+        error.message;
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +82,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    //  await client.close();
   }
 }
 run().catch(console.dir);
